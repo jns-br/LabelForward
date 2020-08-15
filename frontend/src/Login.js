33cd715fd4996dae1d95838450b23ca9554a3b9f
jba <jonas.bauer@fokus.fraunhofer.de>
2020-08-15 10:50:55 +0200

    update login handling

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -1,29 +1,60 @@
 import React, { Component } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import { Redirect} from 'react-router-dom';
 import './Login.css'
 
 class Login extends Component {
 
+  state = {
+    redirect: false,
+    email: "",
+    password:""
+  }
+
+  setRedirect = () => {
+    this.setState({redirect: true});
+  }
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to='/home' />
+    }
+  }
+
+  handleEmailChange = event => {
+    this.setState({
+      email: event.target.value
+    })
+  }
+
+  handlePasswordChange = event => {
+    this.setState({
+      password: event.target.value
+    })
+  }
+
   handleLogin = event => {
-    event.preventDefault();
-    console.log(event.target);
+    console.log('email: ', this.state.email);
+    console.log('password: ', this.state.password)
+    this.setRedirect();
   }
 
   render() {
     return (
       <div className="LoginForm">
+        {this.renderRedirect()}
         <h2 className="LoginHeader">CovidState</h2>
         <Form>
           <Form.Group controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control type="email" placeholder="Enter email" onChange={this.handleEmailChange}/>
           </Form.Group>
           <Form.Group controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Control type="password" placeholder="Password" onChange={this.handlePasswordChange}/>
           </Form.Group>
-          <Button variant="primary" type="submit">
-            Submit
+          <Button variant="primary" type="submit" onClick={this.handleLogin}>
+            Login
         </Button>
         </Form>
       </div>
