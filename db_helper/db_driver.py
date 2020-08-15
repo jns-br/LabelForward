01984767110f688c2d15905067c0abfa11a28295
jba <jonas.bauer@fokus.fraunhofer.de>
2020-08-15 13:14:47 +0200

    finish db helper

--- db_helper/db_driver.py
+++ db_helper/db_driver.py
@@ -28,28 +28,80 @@ def connect():
 
 
 def create_table(conn):
-    statement = """
+    statement_news = """
         CREATE TABLE IF NOT EXISTS news(
             news_id SERIAL PRIMARY KEY,
             category VARCHAR(255) NOT NULL,
             headline VARCHAR(500) NOT NULL,
-            authors VARCHAR (100) NOT NULL,
+            authors VARCHAR (500) NOT NULL,
             link VARCHAR (500) NOT NULL,
-            description VARCHAR (1000) NOT NULL,
+            description VARCHAR (2000) NOT NULL,
             publish_date VARCHAR (50) NOT NULL 
         )
     """
 
+    statement_results = """
+        CREATE TABLE IF NOT EXISTS results(
+            result_id SERIAL PRIMARY KEY,
+            news VARCHAR(2000) NOT NULL,
+            label VARCHAR (100) NOT NULL
+        )
+    """
     try:
         cur = conn.cursor()
-        cur.execute(statement)
+        cur.execute(statement_news)
+        cur.execute(statement_results)
         cur.close()
         conn.commit()
-        print('News table created!')
+        print('Created tables!')
     except (Exception, psycopg2.DatabaseError) as error:
         print('error: ', error)
+
+
+def read_news_json(fn, conn):
+    events = ijson.parse(open(fn), multiple_values=True)
+    cur = conn.cursor()
+
+    doc_counter = 0
+    for prefix, event, value in events:
+        if prefix == 'category':
+            category = value
+
+        if prefix == 'headline':
+            headline = value
+
+        if prefix == 'authors':
+            authors = value
+
+        if prefix == 'link':
+            link = value
+
+        if prefix == 'short_description':
+            description = value
+
+        if prefix == 'date':
+            publish_date = value
+
+        if (prefix, event) == ('', 'end_map'):
+            try:
+                statement = """
+                    INSERT INTO news(category, headline, authors, link, description, publish_date)
+                    VALUES (%s, %s, %s, %s, %s, %s)
+                """
+                cur.execute(statement, (category, headline, authors, link, description, publish_date))
+                doc_counter += 1
+                if doc_counter % 100 == 0:
+                    conn.commit()
+                    print('Inserted news docs: ', doc_counter)
+            except (Exception, psycopg2.DatabaseError) as error:
+                print('error: ', error)
+
+    conn.commit()
+    print('Inserted news docs: ', doc_counter)
+    cur.close()
 
 
 if __name__ == '__main__':
     conn = connect()
     create_table(conn)
+    read_news_json(keys.file_name, conn)
--- db_helper/keys.py
+++ db_helper/keys.py
@@ -4,3 +4,4 @@ host = os.getenv('PG_HOST')
 dbname = os.getenv('PG_DB')
 user = os.getenv('PG_USER')
 password = os.getenv('PG_PASSWORD')
+file_name = 'News_Category_Dataset_v2.json'
