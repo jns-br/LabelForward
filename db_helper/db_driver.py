19a2b51b048e73b3c70ecf6d397f1cedd381ddc6
jba <jonas.bauer@fokus.fraunhofer.de>
2020-08-15 12:40:55 +0200

    add table creation function

--- db_helper/db_driver.py
+++ db_helper/db_driver.py
@@ -27,5 +27,29 @@ def connect():
     return conn
 
 
+def create_table(conn):
+    statement = """
+        CREATE TABLE IF NOT EXISTS news(
+            news_id SERIAL PRIMARY KEY,
+            category VARCHAR(255) NOT NULL,
+            headline VARCHAR(500) NOT NULL,
+            authors VARCHAR (100) NOT NULL,
+            link VARCHAR (500) NOT NULL,
+            description VARCHAR (1000) NOT NULL,
+            publish_date VARCHAR (50) NOT NULL 
+        )
+    """
+
+    try:
+        cur = conn.cursor()
+        cur.execute(statement)
+        cur.close()
+        conn.commit()
+        print('News table created!')
+    except (Exception, psycopg2.DatabaseError) as error:
+        print('error: ', error)
+
+
 if __name__ == '__main__':
     conn = connect()
+    create_table(conn)
