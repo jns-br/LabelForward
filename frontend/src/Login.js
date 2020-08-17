fdbc869e54e00cd8d0681c62cb802aab05fbd3c6
jba <jonas.bauer@fokus.fraunhofer.de>
2020-08-17 16:47:36 +0200

    add pw encryption helper function

--- frontend/src/Login.js
+++ frontend/src/Login.js
@@ -42,19 +42,28 @@ class Login extends Component {
   }
 
   handleLogin = event => {
+    event.preventDefault();
     console.log('email: ', this.state.email);
     console.log('password: ', this.state.password)
     this.setRedirect();
   }
 
   handleSignup = event => {
+    event.preventDefault();
     console.log('email: ', this.state.emai);
     console.log('pw: ', this.state.password);
     console.log('controlpw: ', this.state.controlPassword);
+    console.log(process.env.PW_SALT);
     //send req to backend to check for emai
     //if email exists in accessors, send signup request with hashed pw
   }
-
+  async digestPassword(password) {
+    const pwUint8 = new TextEncoder().encode(password);
+    const hashBuffer = await crypto.subtle.digest('SHA-256', pwUint8);
+    const hashArray = await Array.from(new Uint8Array(hashBuffer));
+    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
+    return hashHex;
+  }
   render() {
     return (
       <Tabs defaultActiveKey="login" id="auth-tab">
@@ -82,19 +91,19 @@ class Login extends Component {
             {this.renderRedirect()}
             <h2 className="SignUpHeader">CovidState Sign Up</h2>
             <Form>
-              <Form.Group controlId="formBasicEmail">
+              <Form.Group controlId="formSignupEmail">
                 <Form.Label>Email address</Form.Label>
                 <Form.Control type="email" placeholder="Enter email" onChange={this.handleEmailChange} />
               </Form.Group>
-              <Form.Group controlId="formBasicPassword">
+              <Form.Group controlId="formSignupPassword">
                 <Form.Label>Enter password</Form.Label>
                 <Form.Control type="password" placeholder="Password" onChange={this.handlePasswordChange} />
               </Form.Group>
-              <Form.Group controlId="formBasicControlPassword">
+              <Form.Group controlId="formSignupControlPassword">
                 <Form.Label>Enter password again</Form.Label>
                 <Form.Control type="password" placeholder="Password" onChange={this.handPasswordControlChange} />
               </Form.Group>
-              <Button variant="primary" type="submit" onClick={this.handleLogin}>
+              <Button variant="primary" type="submit" onClick={this.handleSignup}>
                 Sign Up
               </Button>
             </Form>
