dc500f58aa302f3f885c68ae28748ce212d39718
jba <jonas.bauer@fokus.fraunhofer.de>
2020-08-19 11:03:28 +0200

    add update email route

--- backend/controller/UserController.js
+++ backend/controller/UserController.js
@@ -15,4 +15,18 @@ router.post('/signup', async (req, res) => {
   }
 });
 
+router.post('/email', async (req, res) => {
+  try {
+    const { oldEmail, newEmail, password } = req.body;
+    const isUpdated = await UserRepository.updateEmail(oldEmail, newEmail, password);
+    if(isUpdated) {
+      res.status(200).json();
+    } else {
+      res.status(403).json();
+    }
+  } catch (err) {
+    res.status(500).json({msg: err.message});
+  }
+});
+
 module.exports = router;
