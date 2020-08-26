1c25cc901dfa6006b591eb859c09cd815c44e7bb
jba <jonas.bauer@fokus.fraunhofer.de>
2020-08-26 14:49:39 +0200

    remove useless ctor

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -2,28 +2,6 @@ import axios from 'axios';
 import JWTService from './JWTService';
 
 class AuthService {
-  constructor() {
-    // if axios detects 401 -> logout
-    axios.interceptors.response.use(
-      response => response,
-      async error => {
-        if (error && error.response && error.response.status === 401) await this.logout();
-        return Promise.reject(error);
-      }
-    );
-
-    //if authticated add authorization header
-    /*
-    axios.interceptors.request.use(
-      config => {
-        if (this.isAuthenticated()) config.headers.authorization = JWTService.getJWT();
-        return config;
-      },
-      error => Promise.reject(error)
-    );
-    */
-  }
-
   async login(email, password) {
     try {
       const res = await axios.post('/api/login/', {
@@ -32,6 +10,7 @@ class AuthService {
       }, {
         withCredentials: true
       });
+      return res;
     } catch (err) {
       throw err;
     }
