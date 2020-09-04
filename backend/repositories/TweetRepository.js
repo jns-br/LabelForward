1e810ea244afc0468453926cf0c4029325d09f71
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-04 12:40:34 +0200

    update inserttweet to single label

--- backend/repositories/TweetRepository.js
+++ backend/repositories/TweetRepository.js
@@ -24,10 +24,10 @@ class TweetRepository {
     }
   }
 
-  async insertLabeledTweet(tweet, labels) {
+  async insertLabeledTweet(tweet, label) {
     try {
-      const statement = "INSERT INTO results(news, labels) VALUES($1, $2::text[])";
-      const result = await this.pgClient.query(statement, [tweet, labels]);
+      const statement = "INSERT INTO results(news, labels) VALUES($1, $2)";
+      const result = await this.pgClient.query(statement, [tweet, label]);
       if (result.rowCount !== 1) {
         throw new Error('Insertion failed');
       }
