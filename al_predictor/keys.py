7f3666ddbe47f353ac2f38af24b3ed9dd4ea5335
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-05 08:13:57 +0200

    start predictor

---
+++ al_predictor/keys.py
@@ -0,0 +1,9 @@
+import os
+
+pg_host = os.getenv('PGHOST')
+pg_dbname = os.getenv('PGDATABASE')
+pg_user = os.getenv('PGUSER')
+pg_password = os.getenv('PGPASSWORD')
+pg_port = os.getenv('PGPORT')
+redis_host = os.getenv('REDIS_HOST')
+redis_port = os.getenv('REDIS_PORT')
