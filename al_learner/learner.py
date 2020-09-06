a55e2b7c7f656598428ccdd108d93099a895515e
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-06 16:26:04 +0200

    add majority label call

--- al_learner/learner.py
+++ al_learner/learner.py
@@ -17,7 +17,9 @@ if __name__ == '__main__':
         # read indices, select records to df
         if new_msg['data'] == 'update':
             df = pg_helper.read_labeled_data_full()
-            X = df['news'].to_numpy(dtype=str)
+            #majority vote 
+            df = model_helper.create_majority_label(df)
+            X = df['tweet'].to_numpy(dtype=str)
             y = df['label'].to_numpy()
             clf, id = model_helper.create_model(X, y)
             print('Model created and saved', flush=True)
