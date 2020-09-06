9a8372a887695866362dd71bab9fb6bbe497db15
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-06 13:21:35 +0200

    refactor to ioredis

--- backend/repositories/TweetRepository.js
+++ backend/repositories/TweetRepository.js
@@ -1,5 +1,5 @@
 const keys = require('../keys');
-const redis = require('redis');
+const redis = require('ioredis');
 const { Pool } = require('pg');
 
 class TweetRepository {
@@ -11,10 +11,9 @@ class TweetRepository {
       password: keys.pgPassword,
       port: keys.pgPort
     });
-    this.redisClient = redis.createClient({
-      host: keys.redisHost,
+    this.redisClient = new Redis({
       port: keys.redisPort,
-      retry_strategy: () => 1000
+      host: keys.redisHost
     });
   }
 
@@ -32,26 +31,20 @@ class TweetRepository {
 
   async getNextTweetNew(email) {
     try {
-      this.redisClient.get('queryflag', async(err, queryFlag) => {
-        if (err) {
-          console.log('Redis error', err.message);
-          throw err;
-        }
-
-        switch (queryFlag) {
-          case 'available':
-            const statement = "SELECT tweet FROM queries WHERE NOT ($1 = ANY (users)) ORDER BY (array_length(users, 1)) DESC";
-            const result = await this.pgClient.query(statement, [email]);
-            return result;
-          case 'unavailable':
-            return null;
-          default:
-            this.redisClient.set('queryflag', 'unavailable', (err, reply) => {
-              this.redisClient.publish('predictor', 'update');
-            });
-            return null;
-        }
-      });
+      const queryFlag = await this.redisClient.get('queryflag');
+      switch (queryFlag) {
+        case 'available':
+          const statement = "SELECT tweet FROM queries WHERE NOT ($1 = ANY (users)) ORDER BY (array_length(users, 1)) DESC";
+          const result = await this.pgClient.query(statement, [email]);
+          return result;
+        case 'unavailable':
+          return null;
+        default:
+          this.redisClient.set('queryflag', 'unavailable', (err, reply) => {
+            this.redisClient.publish('predictor', 'update');
+          });
+          return null;
+      }
     } catch (err) {
       console.error('DB error', err.message);
       throw err;
@@ -74,21 +67,18 @@ class TweetRepository {
 
   async insertLabeledTweetNew(tweet, label, email) {
     try {
-      const statement = "UPDATE queries SET labels = array_cat(labels, {$1}), users = array_cat(users, {$2} WHERE tweet = $3";
+      const statement = "UPDATE queries SET labels = array_cat(labels, {$1}), users = array_cat(users, {$2}) WHERE tweet = $3";
       const result = await this.pgClient.query(statement, [label, email, tweet]);
       if (result.rowCount !== 1) {
         throw new Error('Insertion failed');
       }
       const isFull = await this.isQueryTableFull();
       if (isFull) {
-        this.redisClient.set('queryflag', 'unavailable', (setRrr, setReply) => {
-          this.redisClient.publish('predictor', 'update', (pubErr, pubReply) => {
-            return result;
-          })
-        })
-      } else {
-        return result;
+        await this.redisClient.set('querflag', 'unavailable');
+        const pub = this.redisClient.duplicate();
+        await pub.publish('predictor', 'update');
       }
+      return result;
     } catch (err) {
       console.error('DB error', err.message);
       throw err;
@@ -111,7 +101,7 @@ class TweetRepository {
       const statement = "SELECT COUNT(*) AS cnt FROM queries WHERE array_length(labels) <= $1";
       const result = await this.pgClient.query(statement, [keys.minLabelCount]);
       const count = parseInt(result.rows[0].cnt);
-      if (count/keys.setSize >= keys.queryThreshold) {
+      if (count / keys.setSize >= keys.queryThreshold) {
         return true;
       } else {
         return false;
