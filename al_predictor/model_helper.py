0fe276dc43f813c0c31a53f263793c043f30d71f
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-10 11:51:55 +0200

    add get inital batch func

--- al_predictor/model_helper.py
+++ al_predictor/model_helper.py
@@ -16,16 +16,10 @@ def get_last_model():
 
 def make_queries():
     clf = get_last_model()
-    if clf is None:
-        pg_helper.read_batch()
-    else:
-        X = pg_helper.read_all_text().to_numpy()
-        probas = clf.predict_proba(X)
-        highest_probas = np.array([np.argmax(y) for y in probas])
-        data_with_probas = np.c_[X, highest_probas]
-        selection = np.empty(shape=(keys.set_size, 2))
-        for i in range(keys.set_size):
-            lowest_index = np.argmin(data_with_probas[:, 1:], axis=0)
-            selection[lowest_index] = data_with_probas[lowest_index]
-            np.delete(arr=data_with_probas, obj=lowest_index, axis=0)
-        pg_helper.save_queries(selection)
+    df = pg_helper.read_all_text()
+    X = df['tweet'].to_numpy()
+    probas = clf.predict_proba(X)
+    highest_probas = np.array([np.argmax(y) for y in probas])
+    for index, row in df.iterrows():
+        df.iloc[index]['uncertainty'] = highest_probas[index]
+    pg_helper.save_queries(df)
--- al_predictor/predictor.py
+++ al_predictor/predictor.py
@@ -1,14 +1,20 @@
 import redis
-import  keys
+import keys
+import model_helper
+import pg_helper
 
 if __name__ == '__main__':
     print('Started al predictor', flush=True)
     r = redis.Redis(host=keys.redis_host, port=keys.redis_port, decode_responses=True)
     subscriber = r.pubsub()
-    subscriber.subscribe('learner')
-    print('Intialized redis', flush=True)
+    subscriber.subscribe('predictor')
+    print('Initialized redis', flush=True)
 
     for new_msg in subscriber.listen():
         print('Message: ', new_msg['data'], flush=True)
-        if new_msg['data'] == 'getqueries':
-            print('get new queries')
+        if new_msg['data'] == 'update':
+            model_helper.make_queries()
+            r.set('queryDone', 'true')
+        if new_msg['data'] == 'init':
+            pg_helper.get_initial_batch()
+            r.set('queryDone', 'true')
