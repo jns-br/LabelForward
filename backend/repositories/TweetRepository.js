e49362c23c49fb80182d3ae8896237c5c5db1933
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-11 09:55:22 +0200

    fix threshold bug

--- backend/repositories/TweetRepository.js
+++ backend/repositories/TweetRepository.js
@@ -76,7 +76,10 @@ class TweetRepository {
       const statement = "SELECT COUNT(*) AS cnt FROM queries WHERE array_length(labels, 1) >= $1";
       const result = await this.pgClient.query(statement, [keys.minLabelCount]);
       const count = parseInt(result.rows[0].cnt);
-      if (count / parseInt(keys.setSize) >= parseInt(keys.queryThreshold)) {
+      console.log('Count', count);
+      console.log('count/setsize', count/parseInt(keys.setSize));
+      console.log('Threshold', parseFloat(keys.queryThreshold))
+      if (count / parseInt(keys.setSize) >= parseFloat(keys.queryThreshold)) {
         return true;
       } else {
         return false;
