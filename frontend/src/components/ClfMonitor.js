37e3d32a3df34de0e4877943abad4c03ec684a77
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-15 09:52:00 +0200

    add clf monitor

--- frontend/src/components/ClfMonitor.js
+++ frontend/src/components/ClfMonitor.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
+import { Table } from 'react-bootstrap';
 
 class ClfMonitor extends Component {
-
+  render() {
+    return (
+      
+    )
+  }
 }
 
 export default ClfMonitor;
