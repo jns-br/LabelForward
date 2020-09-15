9d65f5a1c46792bead8d63be717de2bd3533a3dc
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-15 09:43:43 +0200

    start monitor frontend

---
+++ frontend/src/components/ClfMonitor.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+class ClfMonitor extends Component {
+
+}
+
+export default ClfMonitor;
---
+++ frontend/src/pages/MonitorPage.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+class Monitor extends Component {
+
+}
+
+export default Monitor;
