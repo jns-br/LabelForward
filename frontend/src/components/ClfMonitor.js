251e69807147e7231b38744ea53f51a4358edcda
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-15 10:10:23 +0200

    implement render func

--- frontend/src/components/ClfMonitor.js
+++ frontend/src/components/ClfMonitor.js
@@ -4,7 +4,22 @@ import { Table } from 'react-bootstrap';
 class ClfMonitor extends Component {
   render() {
     return (
-      
+      <Table striped bordered hover>
+        <thead>
+          <th>Classifier</th>
+          <th>Precision</th>
+          <th>Created at</th>
+        </thead>
+        <tbody>
+          {this.props.clfs.map((value, index) => {
+            <tr>
+              <td key={index}>{value.clfId}</td>
+              <td key={index}>{value.precision}</td>
+              <td key={index}>{value.timestamp}</td>
+            </tr>
+          })}
+        </tbody>
+      </Table>
     )
   }
 }
