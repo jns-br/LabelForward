c4cd00fb9e260aac6f630b0dfa03918d3f42f5d6
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-15 11:30:00 +0200

    add get label share func

--- backend/repositories/MonitorRepository.js
+++ backend/repositories/MonitorRepository.js
@@ -17,17 +17,32 @@ class MonitorRepository {
       const statement = "SELECT clf_id, precision_score, created_at FROM classifiers";
       const result = await this.pgClient.query(statement);
       clfData = result.rows.map(val => {
-        const data = {
+        return {
           clfId : val.clf_id,
           precision : val.precision_score,
-          timestampe : val.created_at
+          timestamp : val.created_at
         }
-        return data;
       });
       return clfData;
     } catch (err) {
       console.error('DB error', err.message);
       throw err;
+    }
+  }
+
+  async getLabelShare() {
+    try {
+      const countStatment = "SELECT COUNT(*) AS cnt FROM data WHERE labeled = true";
+      const countResult = await this.pgClient.query(countStatment);
+      const labelCount = countResult.rows[0].cnt;
+
+      const totalStatement = "SELECT COUNT(*) AS cnt FROM data";
+      const totalResult = await this.pgClient.query(totalStatement);
+      const totalCount = totalResult.rows[0].cnt;
+
+      return labelCount / totalCount;
+    } catch (err) {
+      
     }
   }
 }
