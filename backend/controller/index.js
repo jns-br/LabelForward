629c9da235869ddfda0179517b3ca03532863a76
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-15 11:40:10 +0200

    add monitorcontroller

--- backend/controller/index.js
+++ backend/controller/index.js
@@ -1,6 +1,7 @@
 const TextController = require('./TextController');
 const UserController = require('./UserController');
 const AuthController = require('./AuthController');
+const MonitorController = require('./MonitorController');
 
 const express = require('express');
 const router = express.Router();
@@ -8,5 +9,6 @@ const router = express.Router();
 router.use('/texts', TextController);
 router.use('/users', UserController);
 router.use('/login', AuthController);
+router.use('/monitor', MonitorController);
 
 module.exports = router;
