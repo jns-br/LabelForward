d3cbba413522966d11d3561406e8397c7193d223
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-24 12:08:21 +0200

    add create labels func

--- dl_manager/model_helper.py
+++ dl_manager/model_helper.py
@@ -1,9 +1,21 @@
 import pickle
 import pg_helper
+import pandas as pd
+
 
 def get_clf_by_id(conn, clf_id):
     data = pg_helper.load_model_by_id(conn, clf_id)
     clf = pickle.loads(data)
     return clf
 
-def create_labels(conn, clf_id, df):
+
+def create_labels(conn, clf_id):
+    count_vec = pickle.loads(pg_helper.load_count_vec(conn))
+    clf = get_clf_by_id(conn, clf_id)
+    data_df = pg_helper.load_unlabeled_data(conn)
+    X = count_vec.transform(data_df['text_data'].to_numpy(dtype=str).ravel())
+    labels = clf.predict(X)
+    labels_df = pd.DataFrame(labels, columns=['major_label'])
+    data_df['major_label'] = labels_df['major_label']
+    return data_df
+
