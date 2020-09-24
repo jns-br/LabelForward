4e51a5df9da06011c17ac560ba75a664e7dfa7d5
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-24 14:44:35 +0200

    add post dl route

--- backend/controller/MonitorController.js
+++ backend/controller/MonitorController.js
@@ -2,8 +2,11 @@ const express = require('express');
 const router = express.Router();
 const MonitorRepository = require('../repositories/MonitorRepository');
 const JWTService = require('../services/JWTService');
+const Redis = require('ioredis');
+const keys = require('../keys');
+const { json } = require('body-parser');
 
-router.get('/classifiers', async (req, res) => {
+router.get('/classifiers', JWTService.requireJWT(), async (req, res) => {
   try {
     const clfData = await MonitorRepository.getClfData();
     res.status(200).json({clfData: clfData});
@@ -12,7 +15,7 @@ router.get('/classifiers', async (req, res) => {
   }
 });
 
-router.get('/label', async (req, res) => {
+router.get('/label', JWTService.requireJWT(), async (req, res) => {
   try {
     const labelShare = await MonitorRepository.getLabelShare();
     res.status(200).json({labelShare: labelShare});
@@ -20,5 +23,16 @@ router.get('/label', async (req, res) => {
     res.status(500).json({msg: err.message});
   }
 });
+
+router.post('/download', JWTService.requireJWT(), async (req, res) => {
+  try {
+    const clfMsg = "clf-" + req.body.clfId;
+    const redis = new Redis({ port: keys.redisPort, host: keys.redisHost });
+    await redis.publish('dl_manager', clfMsg);
+    res.status(200).json();
+  } catch (err) {
+    res.status(500),json({msg: err.message});
+  }
+})
 
 module.exports = router;
