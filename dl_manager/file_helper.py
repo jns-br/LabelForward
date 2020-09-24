cc881283f3e00c652482dca5ed3f30d18b45efb5
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-24 12:47:35 +0200

    add create zip func

--- dl_manager/file_helper.py
+++ dl_manager/file_helper.py
@@ -1,4 +1,7 @@
 import pickle
+from zipfile import ZipFile
+import os
+from os.path import basename
 
 
 def save_df_to_csv(fn, clf_id, df):
@@ -10,3 +13,13 @@ def save_clf(clf, clf_id):
     path = '/usr/share/data/' + clf_id + "/clf.p"
     with open(path, 'wb') as handle:
         pickle.dump(clf, handle)
+
+
+def create_zip_file(clf_id):
+    dir_name = '/usr/share/data/' + clf_id
+    zip_name = dir_name + '/data.zip'
+    with ZipFile(zip_name, 'w') as zip_obj:
+        for folder_name, subfolders, filenames in os.walk(dir_name):
+            for filename in filenames:
+                filepath = os.path.join(folder_name, filename)
+                zip_obj.write(filepath, basename(filepath))
