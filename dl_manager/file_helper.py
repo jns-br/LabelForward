f0ffb5acaf7b21ff391df619f30c489913d66850
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-27 07:52:41 +0200

    change data dir

--- dl_manager/file_helper.py
+++ dl_manager/file_helper.py
@@ -6,31 +6,24 @@ from os.path import basename
 
 def save_df_to_csv(fn, clf_id, df):
     try:
-        os.mkdir('/usr/share/data/'+ clf_id +'/')
+        os.mkdir('/app/data/'+ clf_id +'/')
     except FileExistsError as error:
         print(error)
-    path = '/usr/share/data/' + clf_id + "/" + fn + '.csv'
+    path = '/app/data/' + clf_id + "/" + fn + '.csv'
     df.to_csv(path_or_buf=path, mode='w+')
 
 
 def save_clf(clf, clf_id):
-    path = '/usr/share/data/' + clf_id + "/clf.p"
+    path = '/app/data/' + clf_id + "/clf.p"
     with open(path, 'wb') as handle:
         pickle.dump(clf, handle)
 
 
 def create_zip_file(clf_id):
-    dir_name = '/usr/share/data/' + clf_id
-    zip_name = dir_name + '/data-' + clf_id + '.zip'
+    dir_name = '/app/data/' + clf_id
+    zip_name = '/app/data/data-' + clf_id + '.zip'
     with ZipFile(zip_name, 'w') as zip_obj:
         for folder_name, subfolders, filenames in os.walk(dir_name):
             for filename in filenames:
                 filepath = os.path.join(folder_name, filename)
                 zip_obj.write(filepath, basename(filepath))
-
-
-def move_zip(clf_id):
-    filename = 'data-' + clf_id + '.zip'
-    filepath = '/usr/share/data/' + clf_id + '/' + filename
-    new_path = '/usr/share/data/' + filename
-    os.rename(filepath, new_path)
