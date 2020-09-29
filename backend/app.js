a32dbc9a6332b71c940deb639c577fdae99dcc4c
jba <jonas.bauer@fokus.fraunhofer.de>
2020-09-29 13:13:51 +0200

    update cors

--- backend/app.js
+++ backend/app.js
@@ -9,17 +9,16 @@ const Passport = require('./passport');
 
 const app = express();
 app.use(cookieParser());
-app.use(cors({
-  origin: 'http://client:3000',
-  credentials: true
-}));
+
+app.use(cors());
+
 app.use(bodyParser.json());
 
 const port = keys.node_port || 8080;
 
 app.use('/', controller);
 
-app.listen(port, async (err) => {
+app.listen(port, "0.0.0.0", async (err) => {
   await Passport.init();
   console.log(`Server listening at port ${port}`);
 });
