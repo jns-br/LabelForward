19afdcea2231dc3cb1ea8419ddd6b553b8ab741e
jba <jonas.bauer@fokus.fraunhofer.de>
2020-10-01 10:55:24 +0200

    possible 404 fix

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,18 @@ app.use(bodyParser.json());
 
 const port = keys.node_port || 8080;
 
+app.use((req, res, next) => {
+  const allowedOrigins = ['label-forward.demo.datexis.com', 'http://frontend:3000'];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS', 'POST');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
+  return next();
+});
+
 app.use('/', controller);
 
 app.listen(port, "0.0.0.0", async (err) => {
