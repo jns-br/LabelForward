272e6bc7b4bbaf1cce9aa6d225a4b3469cc7b4c3
jba <jonas.bauer@fokus.fraunhofer.de>
2020-10-19 12:21:05 +0200

    update auth controller consts

--- api/constants.js
+++ api/constants.js
@@ -3,7 +3,9 @@ module.exports = {
   keyEmail: 'email',
   keyHostAll: "0.0.0.0",
   routeApi: '/api',
+  routeBase: '/',
   routeLogin: '/login',
+  routeLogout: '/logout',
   routeMonitor: '/monitor',
   routeTexts: '/texts',
   routeUsers: '/users'
