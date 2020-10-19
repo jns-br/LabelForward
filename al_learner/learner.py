40c2d1b8580954d1a2c3ce621b66365aa7d4c5bc
jba <jonas.bauer@fokus.fraunhofer.de>
2020-10-19 10:46:45 +0200

    update constants

--- al_learner/learner.py
+++ al_learner/learner.py
@@ -1,4 +1,5 @@
 import keys
+import constants
 import pg_helper
 import model_helper
 import redis
@@ -9,23 +10,23 @@ if __name__ == '__main__':
     print('Started al learner', flush=True)
     r = redis.Redis(host=keys.redis_host, port=keys.redis_port, decode_responses=True)
     subscriber = r.pubsub()
-    subscriber.subscribe('learner')
+    subscriber.subscribe(constants.key_learner)
     print('Initialized redis', flush=True)
     conn = pg_helper.connect()
     print('Initialized postgres connection', flush=True)
 
     for new_msg in subscriber.listen():
-        print('Message: ', new_msg['data'], flush=True)
-        if new_msg['data'] == 'init':
+        print('Message: ', new_msg[constants.key_data], flush=True)
+        if new_msg[constants.key_data] == constants.msg_init:
             model_helper.init(conn)
-            r.publish('predictor', 'update')
-        if new_msg['data'] == 'update':
+            r.publish(constants.key_predictor, constants.msg_update)
+        if new_msg[constants.key_data] == constants.msg_update:
             batch_ready = pg_helper.is_new_batch_ready(conn)
             if batch_ready:
                 X_test, y_test, clf, id = model_helper.train_label_clf(conn)
                 X_test_ignore, y_test_ignore, clf_ignore, id_ignore = model_helper.train_ignore_clf(conn)
                 if clf is not None:
-                    r.publish('predictor', 'update')
+                    r.publish(constants.key_predictor, constants.msg_update)
                     model_helper.create_precision_score(X_test, y_test, clf, id, conn)
                     if clf_ignore is not None:
                         model_helper.create_precision_score(
