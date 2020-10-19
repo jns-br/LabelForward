506eed31d834054e07b534fd5aed24d6c731115f
jba <jonas.bauer@fokus.fraunhofer.de>
2020-10-19 11:43:19 +0200

    update predictor constants

---
+++ al_predictor/constants.py
@@ -0,0 +1,8 @@
+key_data = 'data'
+key_predictor = 'predictor'
+key_query_counter = 'queryCounter'
+key_query_flag = 'queryFlag'
+
+msg_available = 'available'
+msg_unavailable = 'unavailable'
+msg_update = 'update'
