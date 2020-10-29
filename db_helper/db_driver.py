573db355b8c9d0a7f40209aa0a75c8a250362586
Jonas <Jonas>
2020-10-29 12:46:25 +0100

    fix init error

--- db_helper/db_driver.py
+++ db_helper/db_driver.py
@@ -183,7 +183,7 @@ def check_for_existing_data(conn):
 
 def read_init_data(conn):
     try:
-        init_data = pd.read_json(keys.init_data_path)
+        init_data = pd.read_csv(keys.init_data_path)
         cur = conn.cursor()    
         statement = "INSERT INTO init_data(text_data, label) VALUES (%s, %s) ON CONFLICT DO NOTHING"
 
@@ -196,7 +196,7 @@ def read_init_data(conn):
         conn.commit()
         print('Inserted init data')
         cur.close()
-    except FileNotFoundError:
+    except (ValueError, FileNotFoundError):
         print('No init data found')
 
 
