53b41cd353f2613820fe2a28be93eaaee6d7bd0d
Jonas <Jonas>
2020-11-02 11:49:50 +0100

    add insert start time func

--- api/repositories/TextRepository.js
+++ api/repositories/TextRepository.js
@@ -35,11 +35,14 @@ class TextRepository {
           const uncertainty_threshold = parseFloat(keys.uncertaintyThreshold);
           if (uncertainty_ignore) {
             if (uncertainty_ignore - uncertainty >= uncertainty_threshold) {
+              await this.insertStartTime(email, parseInt(result_ignore.rows[0].text_id));
               return result_ignore.rows[0];
             } else {
+              await this.insertStartTime(email, parseInt(result.rows[0].text_id));
               return result.rows[0];
             }
           } else {
+            await this.insertStartTime(email, parseInt(result.rows[0].text_id));
             return result.rows[0];
           }
           
@@ -83,6 +86,24 @@ class TextRepository {
       const statement = statements.selectLabels;
       const results = await this.pgClient.query(statement);
       return Array.from(results.rows, result => result.label);
+    } catch (err) {
+      console.error('DB error', err.message);
+      throw err;
+    }
+  }
+
+  async insertStartTime(email, textId) {
+    try {
+      const statementUser = 'SELECT user_id FROM users WHERE email = $1'
+      const resultUser = await this.pgClient.query(statementUser, [email]);
+      const userId = parseInt(resultUser.rows[0].user_id);
+
+      const date = Date.now() / 1000;
+      const statementInsert = 'INSERT INTO sample_timestamps(user_id, text_id, start) VALUES($1, $2, to_timestamp($3))';
+      const resultInsert = await this.pgClient.query(statementInsert, [userId, textId, date])
+      if (resultInsert.rowCount !== 1) {
+        throw new Error('Insertion failed');
+      }
     } catch (err) {
       console.error('DB error', err.message);
       throw err;
