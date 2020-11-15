77e825c33f5f0d8c1ce75d08845bdfbcff3a80a5
Jonas <Jonas>
2020-11-15 16:38:10 +0100

    add basic logging

--- al_learner/learner.py
+++ al_learner/learner.py
@@ -3,24 +3,33 @@ import constants
 import pg_helper
 import model_helper
 import redis
+import logging
+import logging.config
+import yaml
 
 
 if __name__ == '__main__':
 
-    print('Started al learner', flush=True)
+    with open('logger-config.yaml', 'r') as f:
+        config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+    
+    logger = logging.getLogger('logger')
+    logger.info('Connecting to Redis')
     r = redis.Redis(host=keys.redis_host, port=keys.redis_port, decode_responses=True)
     subscriber = r.pubsub()
     subscriber.subscribe(constants.key_learner)
-    print('Initialized redis', flush=True)
+    logger.info('Redis connection intialized')
     conn = pg_helper.connect()
-    print('Initialized postgres connection', flush=True)
+    logger.info('Postgres connection initialized')
 
     for new_msg in subscriber.listen():
-        print('Message: ', new_msg[constants.key_data], flush=True)
         if new_msg[constants.key_data] == constants.msg_init:
+            logger.info('Received init message')
             model_helper.init(conn)
             r.publish(constants.key_predictor, constants.msg_update)
         if new_msg[constants.key_data] == constants.msg_update:
+            logger.info('Received update message')
             batch_ready = pg_helper.is_new_batch_ready(conn)
             if batch_ready:
                 X_test, y_test, clf, id = model_helper.train_label_clf(conn)
