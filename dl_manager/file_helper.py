5a404fa643389497657c21f2fc8618987b72f725
Jonas <Jonas>
2020-11-18 11:21:16 +0100

    add logging to file helper

--- dl_manager/file_helper.py
+++ dl_manager/file_helper.py
@@ -2,24 +2,31 @@ import pickle
 from zipfile import ZipFile
 import os
 from os.path import basename
+import logging
 
+logger = logging.getLogger('logger')
 
 def save_df_to_json(fn, clf_id, df):
+    logger.info('Saving dataframe as json')
     try:
         os.mkdir('/app/data/'+ clf_id +'/')
     except FileExistsError as error:
         print(error)
     path = '/app/data/' + clf_id + "/" + fn + '.json'
     df.to_json(path)
+    logger.info('Dataframe saved as json')
 
 
 def save_clf(clf, clf_id):
+    logger.info('Saving classifer as pickle')
     path = '/app/data/' + clf_id + "/clf.p"
     with open(path, 'wb') as handle:
         pickle.dump(clf, handle)
+    logger.info('Classifer saved as pickle')
 
 
 def create_zip_file(clf_id):
+    logger.info('Creating zip file')
     dir_name = '/app/data/' + clf_id
     zip_name = '/app/data/data-' + clf_id + '.zip'
     with ZipFile(zip_name, 'w') as zip_obj:
@@ -27,3 +34,4 @@ def create_zip_file(clf_id):
             for filename in filenames:
                 filepath = os.path.join(folder_name, filename)
                 zip_obj.write(filepath, basename(filepath))
+    logger.info('Zip file created')
