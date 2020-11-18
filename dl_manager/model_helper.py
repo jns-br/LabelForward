d081ba817bdedbdfeae446ae54b593a62291c4d2
Jonas <Jonas>
2020-11-18 18:34:47 +0100

    add handling of all data labeled

--- dl_manager/model_helper.py
+++ dl_manager/model_helper.py
@@ -10,6 +10,8 @@ def create_labels(conn, clf_id):
     count_vec = pickle.loads(pg_helper.load_count_vec(conn))
     clf = pickle.loads(pg_helper.load_model_by_id(conn, clf_id))
     data_df = pg_helper.load_unlabeled_data(conn)
+    if len(data_df.index) == 0:
+        return None, None
     X = count_vec.transform(data_df['text_data'].to_numpy(dtype=str).ravel())
     labels = clf.predict(X)
     labels_df = pd.DataFrame(labels, columns=['major_label'])
