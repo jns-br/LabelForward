c2f4f2e4aa8ff72ff22e1cbd46db3fa019ffe309
Jonas <Jonas>
2020-12-30 11:02:07 +0100

    add insert end time func

--- api/repositories/TimestampRepository.js
+++ api/repositories/TimestampRepository.js
@@ -27,6 +27,21 @@ class TimestampRepository {
       throw err;
     }
   }
+
+  async insertEndTime(email, textId) {
+    try {
+      const statementUser = statements.selectUserByEmail;
+      const resultUser = await this.pgClient.query(statementUser, [email]);
+      const userId = parseInt(resultUser.rows[0].user_id);
+
+      const date = Date.now() / 1000;
+      const statementUpdate = statements.updateTimestamp;
+      await this.pgClient.query(statementUpdate, [date, textId, userId]);
+    } catch (err) {
+      console.error('DB error', err.message);
+      throw err;
+    }
+  }
 }
 
 module.exports = new TextRepository();
